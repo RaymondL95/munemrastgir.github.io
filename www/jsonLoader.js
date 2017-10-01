80a3793ad9770a9b9c5fa5b516393bbaa27c1684
lilchris123 <32070975+lilchris123@users.noreply.github.com>
2017-09-30 20:59:28 -0400

    update

---
+++ www/jsonLoader.js
@@ -0,0 +1,17 @@
+var Data = $.getJSON("fitJsonDataExample.json",function(data){
+    console.log(data);
+     document.getElementById("demo").innerHTML =
+data.Bicep[0].Exercise + " " + data.Bicep[0].link;
+});
+function loadJSON(file){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.overrideMimeType("application/json");
+    xmlhttp.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            json_data = JSON.parse(xmlhttp.responseText);
+          }
+    };
+    xmlhttp.send(null);
+}
