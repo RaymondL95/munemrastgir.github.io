a37c714f133b3a7c5da422b683481d9149977ad1
lilchris123 <32070975+lilchris123@users.noreply.github.com>
2017-10-02 12:50:07 -0400

    updated jsonLoader and json file

--- www/jsonLoader.js
+++ www/jsonLoader.js
@@ -1,13 +1,14 @@
 var Data = $.getJSON("fitJsonDataExample.json",function(data){
     console.log(data);
+    var str= "link";
     document.getElementById("demo").innerHTML =
-data.Bicep[0].Exercise + " " + data.Bicep[0].link;
+data.Bicep[0].Exercise + " " + str.link(data.Bicep[0].link);
     
     document.getElementById("demo1").innerHTML =   
-data.Bicep[1].Exercise + " " + data.Bicep[1].link;
-
+data.Bicep[1].Exercise + " " + str.link(data.Bicep[1].link);
+    
     document.getElementById("demo2").innerHTML =   
-data.Chest[0].Exercise + " " + data.Chest[0].link;
+data.Chest[0].Exercise + " " + str.link(data.Chest[0].link);
 });
 
 function loadJSON(file){
