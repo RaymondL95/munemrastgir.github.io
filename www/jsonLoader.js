577697dd6e11183a7ad7e87d3bb1dbf57763108a
shililiang <32069711+shililiang@users.noreply.github.com>
2017-10-02 12:09:34 -0400

    update to JSON

--- www/jsonLoader.js
+++ www/jsonLoader.js
@@ -1,8 +1,15 @@
 var Data = $.getJSON("fitJsonDataExample.json",function(data){
     console.log(data);
-     document.getElementById("demo").innerHTML =
+    document.getElementById("demo").innerHTML =
 data.Bicep[0].Exercise + " " + data.Bicep[0].link;
+    
+    document.getElementById("demo1").innerHTML =   
+data.Bicep[1].Exercise + " " + data.Bicep[1].link;
+
+    document.getElementById("demo2").innerHTML =   
+data.Chest[0].Exercise + " " + data.Chest[0].link;
 });
+
 function loadJSON(file){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.overrideMimeType("application/json");
