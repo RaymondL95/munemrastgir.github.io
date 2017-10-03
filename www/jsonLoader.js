319342602b152d9519ebd7d3f03e37fc624ca2f8
lilchris123 <32070975+lilchris123@users.noreply.github.com>
2017-10-03 13:35:24 -0400

    merging

--- www/jsonLoader.js
+++ www/jsonLoader.js
@@ -1,25 +1,11 @@
-var Data = $.getJSON("fitJsonDataExample.json",function(data){
+$.getJSON("fitJsonDataExample.json",function(data){
     console.log(data);
-    var str= "link";
-    document.getElementById("demo").innerHTML =
-data.Bicep[0].Exercise + " " + str.link(data.Bicep[0].link);
+    var str= data.Bicep[0].Exercise;
+    document.getElementById("demo").innerHTML =str.link(data.Bicep[0].link);
     
-    document.getElementById("demo1").innerHTML =   
-data.Bicep[1].Exercise + " " + str.link(data.Bicep[1].link);
+    str= data.Bicep[1].Exercise;
+    document.getElementById("demo1").innerHTML =str.link(data.Bicep[1].link);
     
-    document.getElementById("demo2").innerHTML =   
-data.Chest[0].Exercise + " " + str.link(data.Chest[0].link);
+    str= data.Chest[0].Exercise;
+    document.getElementById("demo2").innerHTML =str.link(data.Chest[0].link);
 });
-
-function loadJSON(file){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.overrideMimeType("application/json");
-    xmlhttp.open('GET', file, true); // Replace 'my_data' with the path to your file
-    xmlhttp.onreadystatechange = function () {
-          if (xmlhttp.readyState == 4 && xmlhttp.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            json_data = JSON.parse(xmlhttp.responseText);
-          }
-    };
-    xmlhttp.send(null);
-}
