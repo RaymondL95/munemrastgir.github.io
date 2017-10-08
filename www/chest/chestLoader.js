4572b026643bfeefce13f304871eb6d57533af4b
lilchris123 <32070975+lilchris123@users.noreply.github.com>
2017-10-08 17:43:14 -0400

    Major update
    we no longer need to add tags for each new exercise we are trying to display.
    our js loader will automatically make a unordered list with all the exercises in our json file. Also we need to create the style for <ul> as a class because we dont want the that list to be style like the menu

--- www/chest/chestLoader.js
+++ www/chest/chestLoader.js
@@ -2,33 +2,25 @@ function exerciseLoader(data){
        document.getElementById("h2").innerHTML= "NICE";//data.Chest[0].Exercise;
 }
 
-function exerciseList(data){
+//this fuction will create a unordered html list of all our exersices loaded from the json file
+function exerciseList(data){ 
     var ul = document.getElementById("list");
+    var str;
     
-    for(i=0; i<=data.Chest.length; i++)
+    for(i=0; i<data.Chest.length; i++)
         {
+            str=data.Chest[i].Exercise;
             var li= document.createElement("li");
-            li.appendChild(document.createTextNode(data.Chest[i].Exercise + " Secondary muscle: " + " " + data.Chest[i].Secondary + " " + data.Chest[i].Descriptions));
+            li.innerHTML=str.link(data.Chest[i].link) + " Secondary muscle: " + " " + data.Chest[i].Secondary + " " + data.Chest[i].Descriptions;
             ul.appendChild(li);
         }
 }
 
 $.getJSON("chest.json",function(data){
-    console.log(data);
-    
-    var str= data.Chest[0].Exercise;
-    document.getElementById("Chest").innerHTML = data.Chest[0].Exercise  + " Secondary muscle: " + " " + data.Chest[0].Secondary + " " + data.Chest[0].Descriptions;
-    
-    str= data.Chest[1].Exercise;
-    document.getElementById("Chest1").innerHTML =str.link(data.Chest[1].link)  + " Secondary muscle: " + " " + data.Chest[1].Secondary + " " + data.Chest[1].Descriptions;
-    
-    str= data.Chest[2].Exercise;
-    document.getElementById("Chest2").innerHTML =str.link(data.Chest[2].link)  + " Secondary muscle: " + " " + data.Chest[2].Secondary + " " + data.Chest[2].Descriptions;
-    
+
+    exerciseList(data);
     //document.getElementById("h2").setAttribute("type","button");
     //document.getElementById("h2").onclick=exerciseLoader(data);
-    //document.getElementById("h2").addEventListener("mouseover", exerciseLoader(data));
-    exerciseList(data);
-    
+    //document.getElementById("h2").addEventListener("mouseover", exerciseLoader(data));    
     
 });
