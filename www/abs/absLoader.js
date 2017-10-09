5868520ef197efbbe9235ce1dfe0cf9d1fbff494
shililiang <32069711+shililiang@users.noreply.github.com>
2017-10-08 20:29:49 -0400

    update to JSON

--- www/abs/absLoader.js
+++ www/abs/absLoader.js
@@ -2,10 +2,10 @@ $.getJSON("abs.json",function(data){
     console.log(data);
 
     var str= data.Abs[0].Exercise;
-    document.getElementById("Abs").innerHTML =str.link(data.Abs[0].link) + " " + data.Abs[0].Descriptions +  data.Arms[0].Secondary;
+    document.getElementById("Abs").innerHTML =str.link(data.Abs[0].link) + " " + data.Abs[0].Descriptions;
     
     str= data.Abs[1].Exercise;
-    document.getElementById("Abs1").innerHTML =str.link(data.Abs[1].link) + " " + data.Abs[1].Descriptions + data.Arms[1].Secondary;
+    document.getElementById("Abs1").innerHTML =str.link(data.Abs[1].link) + " " + data.Abs[1].Descriptions;
     
     
 });
